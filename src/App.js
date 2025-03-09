e41cb7d8ecbd774f22f9b028f660c7939918fb56
suzaal <suzaalproff@gmail.com>
2025-03-08 23:35:16 -0500

    added few css and Random card generator function along with buttons to check if the code is running in the DOM

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const suits = ["♠", "♣", "♥", "♦"];
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-const deck = [];
+function DeckofCard() {
+  const suits = ["♠", "♣", "♥", "♦"];
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const deck = [];
 
-
-for ( let i = 0; i < 4; i++){
-  for ( let j = 0; j < 13; j++){
-    deck.push( values[j] + suits[i]);
+  //Deck of Cards
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 13; j++) {
+      deck.push(values[j] + suits[i]);
+    }
   }
+
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  //Random Card Generator
+  const RandomCardGenerator = () => {
+    const randomIndex = Math.floor(Math.random() * deck.length);
+    setSelectedCard(deck[randomIndex]);
+  };
+
+  return (
+    <div className="deck-container">
+      <button onClick={RandomCardGenerator} >RandomCard</button>
+      {selectedCard && <p >You selected: {selectedCard}</p>}
+    </div>
+  );
 }
-console.log(deck);
+
+export default DeckofCard;
